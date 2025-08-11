2b795487940049510dbc74e1769e29ef1f223d4b
Ashoky412 <ashoky412@gmail.com>
2025-08-11 16:54:25 +0530

    added server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-const http = require('http'); const port = 9090;
-http.createServer((req,res)=>{res.end('Ashok Projects '+new Date().toISOString())})
-.listen(port, ()=>console.log('Listening on', port));
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 9090;
+
+http.createServer((req,res)=>{
+  let p = req.url === '/' ? '/index.html' : req.url;
+  let filePath = path.join(__dirname, p);
+  fs.readFile(filePath, (err, data) => {
+    res.writeHead(err ? 404 : 200, {'Content-Type': 'text/html'});
+    res.end(err ? 'Not found' : data);
+  });
+}).listen(port, ()=>console.log('Listening on', port));
